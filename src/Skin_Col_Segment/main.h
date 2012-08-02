ce1e05651763da424826f162a38a9946a4fe9d76
AtsushiUMAKATSU <mamamatz@gmail.com>
2012-08-02 16:21:32 +0900

    first commit

---
+++ src/Skin_Col_Segment/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "HandRegion.h"
+#include <time.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#define KINECT_CONFIG_FILENAME "../KinectConfig.xml"
+
+int64   _PrevTickCount;
+
+// Camera capture
+CvCapture * gCapture = 0;
+
+//
+// Images
+//
+IplImage * image = 0;
+IplImage * gray = 0;
+IplImage * hand_region;
+
+HandRegion _HandRegion;
+
+#endif
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "UserConstant.h"
+
+extern bool running;
+
+void registerMarker();
+void CreateOSGSphereProxy();
+void DeleteVirtualObject(const int & index);
+
+#endif
